95a9befff37b2ad3209b20b76e6df07de9fa0585
Rohan Uttamsingh <rohanuttamsingh@gmail.com>
2021-06-08 23:33:09 -0400

    Basic components created

--- capital-one-ses-movies-challenge/src/app/app.module.ts
+++ capital-one-ses-movies-challenge/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchBarComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule,
