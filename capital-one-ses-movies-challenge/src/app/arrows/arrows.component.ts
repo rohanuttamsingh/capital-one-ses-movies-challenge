4227ac9c84f9e65099da721e76c369cf87cd1111
Rohan Uttamsingh <rohanuttamsingh@gmail.com>
2021-06-15 23:11:13 -0400

    Previous movies button disables when there are no previous pages of movies

--- capital-one-ses-movies-challenge/src/app/arrows/arrows.component.ts
+++ capital-one-ses-movies-challenge/src/app/arrows/arrows.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ApiService } from '../services/api.service';
-import { Movie } from '../models/movie.model';
 
 @Component({
   selector: 'app-arrows',
@@ -9,19 +8,18 @@ import { Movie } from '../models/movie.model';
   styleUrls: ['./arrows.component.scss']
 })
 export class ArrowsComponent implements OnInit {
-  // Should update based on if a search has pages before and after the current one
-  hasPreviousMovies = true;
+  /**
+   * There is always a previous page of movies unless the user is on the first page of results.
+   */
+  get hasPreviousMovies() {
+    return this.apiService.currentPage > 1;
+  }
+
   hasNextMovies = true;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
-    // Updates movies to display whenever alerted by the ApiService
-    this.apiService.moviesChanged.subscribe(
-      (newMovies: Movie[]) => {
-        // this.movies = newMovies;
-      }
-    );
   }
 
   onClickPrevious() {
